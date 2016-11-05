d0e170506b8e4756261d6e5d0375ef7f8d891f0d
George Walker <george.walker1@outlook.com>
2016-11-05 18:17:35 +0000

    adding visualise type, in progress

--- dataviz/MySourceFiles/graph.py
+++ dataviz/MySourceFiles/graph.py
@@ -77,8 +77,45 @@ def visualize_days():
     plt.savefig("Days.png")
     #close plot file
     plt.clf()
+
+
+def visualize_type():
+    """Visualize data by category in a bar graph"""
+
+    # grab our parsed data
+    data_file = parse(MY_FILE, ",")
+    # make a new variable, 'counter', from iterating through each line
+    # of data in the parsed data, and count how many incidents happen
+    # by category
+    counter = Counter(item["Category"] for item in data_file)
+    # Set the labels which are based on the keys of our counter.
+    # Since order doesn't matter, we can just used counter.keys()
+    labels = tuple(counter.keys())
+    # Set exactly where the labels hit the x-axis
+    xlocations = np.arange(len(labels)) + 0.5
+    # Width of each bar that will be plotted
+    width = 0.5
+    # Assign data to a bar plot (similar to plt.plot()!)
+    plt.bar(xlocations, counter.values(), width = width)
+    # Assign labels and tick location to x-axis
+    plt.xticks(xlocations + width/2, labels, rotation = 90)
+
+    # Give some more room so the x-axis labels aren't cut off in the
+    # graph
+    plt.subplots_adjust(bottom=0.4)
+
+    # Make the overall graph/figure is larger
+    plt.rcParams("figure.figsize") = 12, 8
+    # Save the graph!
+    plt.savefig("Type.png")
+
+    # Close plot figure
+    plt.clf()
+
 def main():
-    visualize_days()
+   # visualize_days()
+   visualize_type()
+
 """
 def main():
     new_data = parse(MY_FILE, ",")
